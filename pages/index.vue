351097ebb7140280708328d3059918a21d57e6d8
Gürkan Köse <gurkankose1967@gmail.com>
2021-06-08 17:35:09 +0300

    Create Dashboard page

--- pages/index.vue
+++ pages/index.vue
@@ -7,51 +7,7 @@
         </h1>
       </div>
 
-      <div class="col col-12 col--2xl-12 u-padding-right@2xl-up u-margin-bottom">
-        <form @submit.prevent="addNewTask">
-          <Input
-            v-model="todoInputModel"
-            tag="input"
-            input-element="input"
-            input-type="text"
-            :has-label-text="false"
-            placeholder="Not Alın"
-          />
-        </form>
-      </div>
-
-      <div class="col col-12 col--2xl-12 u-padding-right@2xl-up u-margin-bottom-small@xl-down">
-        <div class="u-display-flex u-justify-content-space-between u-align-items-center">
-          <div>
-            <h2 class="u-margin-0">Yapılacaklar</h2>
-          </div>
-
-          <div v-if="todoList.length >= 1" class="o-todo-filter">
-            <v-select
-              v-model="selectedFilter"
-              :options="['Tümü', 'Tamamlanmışlar', 'Devam Edenler']"
-              class="u-margin-right-small"
-            />
-          </div>
-        </div>
-
-        <ul v-if="todoList.length >= 1" class="o-todo-list">
-          <ListItem
-            v-for="item in todoList"
-            :id="item.id"
-            :key="item.id"
-            :task-name="item.text"
-            :is-done="item.isDone"
-            @taskStatusChange="onTaskStatusChange"
-            @taskDelete="onTaskDelete"
-          />
-        </ul>
-
-        <div v-if="todoList.length < 1" class="u-margin-top">
-          <svg-icon name="IconAlert" title="Uyarı" class="u-color-warning u-font-size-medium" />
-          Yapılacaklar listesi boş
-        </div>
-      </div>
+      <div class="col col-12 col--2xl-12 u-padding-right@2xl-up u-margin-bottom">Ana sayfa</div>
     </div>
   </div>
 </template>
@@ -60,68 +16,10 @@
 export default {
   layout: 'page',
 
-  data() {
-    return {
-      selectedCategory: 'todoApp',
-      selectedFilter: 'Tümü',
-      todoInputModel: '',
-      todoList: [],
-    };
-  },
-
   mounted() {
     this.$nextTick(() => {
       this.$nuxt.$loading.finish();
     });
   },
-
-  methods: {
-    addNewTask(e) {
-      e.preventDefault();
-
-      if (this.todoInputModel == '') {
-        return;
-      }
-
-      let newTask = {
-        id: this.todoInputModel + this.todoList.length,
-        text: this.todoInputModel,
-        isDone: false,
-      };
-
-      this.todoList.push(newTask);
-      this.todoInputModel = '';
-    },
-
-    onTaskStatusChange(id, status) {
-      let item = this.todoList.find(i => i.id == id);
-
-      if (item) {
-        item.isDone = status;
-      }
-    },
-
-    onTaskDelete(id) {
-      let index = this.todoList.findIndex(i => i.id == id);
-
-      if (index > -1) {
-        this.todoList.splice(index, 1);
-      }
-
-      console.log(this.todoList);
-    },
-  },
 };
 </script>
-
-<style lang="scss" scoped>
-@import '../assets/styles/abstracts/index';
-
-.o-todo-list {
-  @include list-unstyled();
-}
-
-.o-todo-filter {
-  min-width: px-to-rem(200px);
-}
-</style>

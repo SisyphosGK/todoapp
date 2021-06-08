1f5093f174b51fd3bf450cd16f4317f6e798d11d
Gürkan Köse <gurkankose1967@gmail.com>
2021-06-08 17:37:17 +0300

    Edit Navbar component for mobile

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,29 +1,50 @@
 <template>
-  <nav class="c-navbar">
-    <div class="c-navbar__container">
-      <button
-        v-for="(link, i) in navLinkDatas"
-        :key="i"
-        type="button"
-        :class="[
-          'c-navbar__link',
-          'u-cursor-pointer',
-          'u-text-align-left',
-          link.path === $route.path ? 'is-active' : null,
-        ]"
+  <transition name="slide-x">
+    <nav v-if="isVisible" class="c-navbar">
+      <Button
+        theme="ghost"
+        :is-circle="true"
+        class="c-navbar__close u-display-none@lg-up"
+        tag="button"
+        @click.native="toggleNavbarVisibility"
       >
-        <svg-icon name="IconWallet" :title="link.name" /> {{ link.name }}
-      </button>
-    </div>
-  </nav>
+        <svg-icon name="IconCross" title="Kapat" />
+      </Button>
+
+      <div class="c-navbar__container">
+        <div>
+          <button
+            v-for="(link, i) in navLinkDatas"
+            :key="i"
+            type="button"
+            :class="[
+              'c-navbar__link',
+              'u-text-align-left@lg-up',
+              link.path === $route.path ? 'is-active' : null,
+            ]"
+          >
+            <svg-icon name="IconDot" :title="link.name" /> {{ link.name }}
+          </button>
+        </div>
+
+        <Button theme="primary" class="u-margin-bottom" tag="NuxtLink" :to="ROUTE_NAMES.HOME.PATH">
+          <svg-icon name="IconPlus" title="Yeni proje oluştur" /> Yeni proje oluştur
+        </Button>
+      </div>
+    </nav>
+  </transition>
 </template>
 
 <script>
 import { ROUTE_NAMES } from '~/project-constants/routeNames';
+import { mapGetters } from 'vuex';
+import STORE_GENERAL from '~/store/general/constants';
 
 export default {
   data() {
     return {
+      isVisible: false,
+
       ROUTE_NAMES,
 
       navLinkDatas: [
@@ -41,6 +62,28 @@ export default {
         },
       ],
     };
+  },
+
+  computed: {
+    watchNavbarVisibleStatus() {
+      return this.getNavbarVisibleStatus();
+    },
+  },
+
+  watch: {
+    watchNavbarVisibleStatus() {
+      this.toggleNavbarVisibility();
+    },
+  },
+
+  methods: {
+    ...mapGetters({
+      getNavbarVisibleStatus: `${STORE_GENERAL.BASE}/${STORE_GENERAL.GETTERS.GET_NAVBAR_VISIBLE_STATUS}`,
+    }),
+
+    toggleNavbarVisibility() {
+      this.isVisible = !this.isVisible;
+    },
   },
 };
 </script>

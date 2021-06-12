d88d4abe9ba4e4114b81e39a63604b2ff0f8304c
Gürkan Köse <gurkankose1967@gmail.com>
2021-06-12 14:19:45 +0300

    Edit LOGIN_MUTATION

--- graphql/mutations/index.js
+++ graphql/mutations/index.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const LOGIN_MUTATION = gql`
   mutation loginMutation($email: String!, $password: String!) {
-    login(input: { email: $email, password: $password }) {
+    login(email: $email, password: $password) {
       access_token
       token_type
       expires_in

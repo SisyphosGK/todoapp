6a662ff2986381de9d2cadcc1cd0256de0fec7da
Gürkan Köse <gurkankose1967@gmail.com>
2021-06-14 17:53:36 +0300

    Create FileUpload component

---
+++ components/FileUpload/FileUpload.vue
@@ -0,0 +1,86 @@
+<template>
+  <div
+    :class="['c-file-upload', file ? 'c-file-upload--uploaded' : null]"
+    @dragover="dragOver"
+    @drop="drop"
+  >
+    <label class="c-file-upload__label">
+      <input
+        ref="file"
+        type="file"
+        name="fields[assetsFieldHandle][]"
+        class="u-display-none"
+        accept=".pdf,.jpg,.jpeg,.png"
+        @change="fileUploaded"
+      />
+
+      <div class="c-file-upload__text">
+        <svg-icon name="IconUpload" title="Dosya Yükle" />
+        Resim yüklemek için sürükleyip bırakın
+      </div>
+    </label>
+
+    <div v-if="file" class="c-file-upload__uploaded-file">
+      <img ref="qwe" src="#" class="c-file-upload__image" />
+
+      <button
+        type="button"
+        title="Dosyayı Sil"
+        class="c-file-upload__remove-image"
+        @click="removeFile()"
+      >
+        <svg-icon :name="`IconCross`" title="Kapat" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: null,
+      fileSrc: null,
+    };
+  },
+
+  methods: {
+    fileUploaded() {
+      this.file = this.$refs.file.files[0];
+      const reader = new FileReader();
+
+      this.$nextTick(() => {
+        const myImg = this.$refs.qwe;
+
+        reader.addEventListener('load', function () {
+          console.log(myImg.src);
+          myImg.src = this.result;
+        });
+        reader.readAsDataURL(this.file);
+
+        this.$emit('fileUploaded', this.file);
+      });
+    },
+
+    removeFile() {
+      this.file = null;
+      this.$emit('fileUploaded', this.file);
+    },
+
+    dragOver(event) {
+      event.preventDefault();
+    },
+
+    drop(event) {
+      event.preventDefault();
+      event.dataTransfer.clearData();
+      this.$refs.file = event.dataTransfer.files[0];
+      this.fileUploaded();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './FileUpload';
+</style>
